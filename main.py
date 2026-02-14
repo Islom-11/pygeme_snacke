efa7d03b6ef120ada88a479f32f7a6d5f7a5d071
Islom-11 <islomnazarboyev55@gmail.com>
2026-02-14 17:07:24 +0500

    Yuklash

---
+++ main.py
@@ -0,0 +1,71 @@
+# import math 
+# a=4096
+# print(math.sqrt(a))
+
+
+# import math 
+# n=[2,4,6,8]
+# print(sum(n) / len(n))
+
+
+
+# a=9
+# print(pow(9,2))
+# print(round(a))
+
+
+# import math
+# a=90
+# b=50
+# print(math.gcd(a,b))
+# print(math.lcm(a,b))
+
+# import math
+# n=90
+# print(math.sin(n))
+# print(math.cos(n))
+# print(math.tan(n))
+
+# b=0
+# for n in range(1, 10):
+#     a = n ** 2
+#     b += a
+#     print(f"{n} ning kvadrati = {a}, yig‘indisi = {b}")
+# print(b)
+
+# a=60
+# b=21
+# print(divmod(a,b))
+
+
+
+
+# talabalar = {
+#     "Ali":95,
+#     "Vali":78,
+#     "Gulbahor":92,
+#     "Jasur": 60
+# }
+# ism = Vali
+# print(talabalar.get(Vali, "Bunday talaba yo'q"))
+
+
+# talabalar = {
+#     "Ali":95,
+#     "Vali":78,
+#     "Gulbahor":92,
+#     "Jasur": 60
+# }
+# talabalar.update({"Islom":80})
+# print(talabalar)
+
+
+talabalar = {
+    "Ali":95,
+    "Vali":78,
+    "Gulbahor":92,
+    "Jasur": 60
+}
+
+talabalar.popitem
+print(talabalar)
